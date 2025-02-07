12810361ad936bb24c327b90cdc877b12c63d2a2
Eris <is-por@hotmail.com>
2025-02-07 21:30:44 +0100

    filtro uwu

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function filterGallery(button, type)
+{
+    var gallery = document.getElementById("gallery");
+    var images = gallery.getElementsByTagName("img");
+
+    for (var i = 0; i < images.length; i++) 
+    {
+        if(type == null)
+        {
+            images[i].classList.remove("hidden");
+        }
+        else if( images[i].classList.contains(type))
+        {
+            images[i].classList.remove("hidden");
+        }else
+        {
+            images[i].classList.add("hidden");
+        }
+    }
+
+    var filterButtons = document.getElementById("gallery-filters");
+    var selectedButtons = filterButtons.getElementsByClassName("selected");
+    for (var i = 0; i < selectedButtons.length; i++) 
+    {
+        selectedButtons[i].classList.remove("selected");
+    }
+
+    button.classList.add("selected");
+}
