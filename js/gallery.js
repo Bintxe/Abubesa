5010c3a5c582e255efca80b6e8c2ec62710e3023
Eris <is-por@hotmail.com>
2025-02-08 18:27:06 +0100

    navegacion :3

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,6 @@
+//Global variables
+var sortedImageList = [];
+
 //Functions
 function filterGallery(button, type)
 {
@@ -19,14 +22,24 @@ function filterGallery(button, type)
         }
     }
 
-    let filterButtons = document.getElementById("gallery-filters");
-    let selectedButtons = filterButtons.getElementsByClassName("selected");
-    for (let i = 0; i < selectedButtons.length; i++) 
-    {
-        selectedButtons[i].classList.remove("selected");
-    }
+    sortedImageList = [...images].sort((a, b) => a.getBoundingClientRect().top > b.getBoundingClientRect().top ? 1 : -1)
+                             .filter(function(obj){ return !obj.classList.contains("hidden") });
 
-    button.classList.add("selected");
+
+    //sortedImageList.forEach((element) => console.log(element));
+
+    if(button)
+    {
+        let filterButtons = document.getElementById("gallery-filters");
+        let selectedButtons = filterButtons.getElementsByClassName("selected");
+        for (let i = 0; i < selectedButtons.length; i++) 
+        {
+            selectedButtons[i].classList.remove("selected");
+        }
+    
+        button.classList.add("selected");
+    }
+    
 }
 
 function onGalleryImageClick(image){
@@ -45,6 +58,33 @@ function onGalleryImageClick(image){
 
     let overlay = document.getElementById("gallery-overlay");
     overlay.classList.remove("hidden");
+
+    document.addEventListener("keydown", checkArrowKeys);
+}
+
+function navigateImages(dir){
+    let overlay = document.getElementById("gallery-overlay");
+    if(overlay.classList.contains("hidden")) return;
+
+    let imgOverlay = document.getElementById("overlay-image");
+
+    let currentIndex = sortedImageList.findIndex(a => a.src === imgOverlay.src) + dir;
+
+    if(currentIndex == sortedImageList.length) currentIndex = 0;
+    else if(currentIndex < 0) currentIndex = sortedImageList.length - 1;
+
+    onGalleryImageClick(sortedImageList[currentIndex]);
+}
+
+function checkArrowKeys(event){
+    console.log("check key")
+    switch (event.key) {
+        case 'ArrowLeft':
+            navigateImages(-1);
+            break;
+        case 'ArrowRight':
+            navigateImages(1);
+    }
 }
 
 function showImageTextOverlay(show){
@@ -56,35 +96,80 @@ function showImageTextOverlay(show){
         textOverlay.style.bottom = -textOverlay.clientHeight+"px";
         textOverlay.style.opacity = 0;
     }
-    
 }
 
-//Execute
-window.addEventListener('scroll', () => {updateNavbarOnScroll()});
+function sortImages(columncount){
+    let gallery =  document.getElementById("gallery");
 
-let gallery = document.getElementById("gallery");
-let images = gallery.getElementsByTagName("img");
+    let columns = [];
+    let columnWidth = (gallery.clientWidth / 5) - (window.innerWidth * 0.01) ;
 
-for (let i = 0; i < images.length; i++) 
+    console.log(gallery.clientWidth+"/5 - "+window.innerWidth+"*0.01 = "+columnWidth);
+    for (let i = 0; i < columncount; i++) 
+    {
+        //initialize columns (0 height for each)
+        columns.push(200)
+    }
+
+    let imageList = gallery.children;
+
+    for (let i = 0; i < imageList.length; i++) 
+    {
+        if(imageList[i].classList.contains("hidden")) continue;
+
+        console.log("image: "+imageList[i].src)
+        let min = Math.min.apply(null, columns);
+        let index = columns.indexOf(min);
+        let xpos = index * columnWidth;
+
+        columns[index] += imageList[i].clientHeight + window.innerWidth * 0.01;
+
+        
+        imageList[i].setAttribute("style", "width:"+(columnWidth) +"px !important;");
+        imageList[i].style.position = "absolute";
+        imageList[i].style.left = xpos+"px";
+        imageList[i].style.top = min+"px";
+    }
+
+}
+
+//Main execution cycle
+window.onload = (event) =>
 {
+    window.addEventListener('scroll', () => {updateNavbarOnScroll()});
+
+    let gallery = document.getElementById("gallery");
+    let images = gallery.getElementsByTagName("img");
+
+    for (let i = 0; i < images.length; i++) 
+    {
+        
+        let img = images[i];
+        images[i].addEventListener('click', function(){onGalleryImageClick(img);});
+    }
+
+    let overlay = document.getElementById("gallery-overlay");
+    let overlayBg = document.getElementById("overlay-background");
+
+    overlayBg.addEventListener('click', function(){ 
+        document.getElementsByTagName("html")[0].style.overflow = "auto";
+        overlay.classList.add("hidden"); 
+        document.removeEventListener("keydown", checkArrowKeys);
+    });
+
+    let overlayImg = overlay.getElementsByClassName("overlay-image-box")[0];
+    overlayImg.addEventListener('mouseover', function(){
+        showImageTextOverlay(true);
+    });
+
+    overlayImg.addEventListener('mouseout', function(){
+        showImageTextOverlay(false);
+    });
+
+    //sortImages(5);
+
+    filterGallery(null, 'sfw');
+
     
-    let img = images[i];
-    images[i].addEventListener('click', function(){onGalleryImageClick(img);});
+
 }
-
-let overlay = document.getElementById("gallery-overlay");
-let overlayBg = document.getElementById("overlay-background");
-
-overlayBg.addEventListener('click', function(){ 
-    document.getElementsByTagName("html")[0].style.overflow = "auto";
-    overlay.classList.add("hidden"); 
-});
-
-let overlayImg = overlay.getElementsByClassName("overlay-image-box")[0];
-overlayImg.addEventListener('mouseover', function(){
-    showImageTextOverlay(true);
-});
-
-overlayImg.addEventListener('mouseout', function(){
-    showImageTextOverlay(false);
-});
