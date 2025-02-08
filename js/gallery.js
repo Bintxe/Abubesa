6c9c426bdaef3ad80bb7d2556e50c92917271e18
Eris <is-por@hotmail.com>
2025-02-08 15:57:47 +0100

    gallery text

--- js/gallery.js
+++ js/gallery.js
@@ -31,7 +31,15 @@ function filterGallery(button, type)
 
 function onGalleryImageClick(image){
     let imgOverlay = document.getElementById("overlay-image");
-    imgOverlay.src=image;
+    imgOverlay.src=image.src;
+
+    let titleOverlay = document.getElementById("overlay-text-title");
+    let yearOverlay = document.getElementById("overlay-text-year");
+    let commOverlay = document.getElementById("overlay-text-extra");
+
+    titleOverlay.innerHTML = image.alt;
+    yearOverlay.innerHTML = image.dataset.year;
+    commOverlay.innerHTML = image.classList.contains("commission") ? "Commission" : "";
 
     document.getElementsByTagName("html")[0].style.overflow = "hidden";
 
@@ -39,8 +47,19 @@ function onGalleryImageClick(image){
     overlay.classList.remove("hidden");
 }
 
-//Execute
+function showImageTextOverlay(show){
+    let textOverlay =  document.getElementById("overlay-image-text");
+    if(show){
+        textOverlay.style.bottom = "0";
+        textOverlay.style.opacity = 1;
+    }else{
+        textOverlay.style.bottom = -textOverlay.clientHeight+"px";
+        textOverlay.style.opacity = 0;
+    }
+    
+}
 
+//Execute
 window.addEventListener('scroll', () => {updateNavbarOnScroll()});
 
 let gallery = document.getElementById("gallery");
@@ -49,7 +68,7 @@ let images = gallery.getElementsByTagName("img");
 for (let i = 0; i < images.length; i++) 
 {
     
-    let img = images[i].src;
+    let img = images[i];
     images[i].addEventListener('click', function(){onGalleryImageClick(img);});
 }
 
@@ -59,4 +78,13 @@ let overlayBg = document.getElementById("overlay-background");
 overlayBg.addEventListener('click', function(){ 
     document.getElementsByTagName("html")[0].style.overflow = "auto";
     overlay.classList.add("hidden"); 
+});
+
+let overlayImg = overlay.getElementsByClassName("overlay-image-box")[0];
+overlayImg.addEventListener('mouseover', function(){
+    showImageTextOverlay(true);
+});
+
+overlayImg.addEventListener('mouseout', function(){
+    showImageTextOverlay(false);
 });
