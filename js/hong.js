0be40b6e7c32221cf4b2fafc35f32ef0ea961fde
Eris <is-por@hotmail.com>
2025-02-26 17:20:32 +0100

    descripciones + backup

--- js/hong.js
+++ js/hong.js
@@ -1,11 +1,20 @@
 //Global variables
-var descHong = ["RAVEN", "HONG", "56", "[REDACTED]","<p>Everyone around Lake Kivu knows who the forest hermit is, but no one really knows her.</p><p>A martial arts master and survival expert, Raven Hong will take on any guerrilla or army single-handedly to defend her people no matter how ruthless and cruel they may be.</p><p>Memories of her past make her realize daily that she was not so different from the people she fights against.</p>"];
-var descMagambo = ["", "MAGAMBO", "??", "aaaaaa", "<p>blablabla</p>"];
-var descEbba = ["", "EBBA", "??", "aaaaaa","<p>blebleble</p>"];
-var descThulani = ["", "THULANI", "??", "aaaaaa","<p>bliblibli</p>"];
-var descAsh = ["", "ASH", "??", "aaaaaa","<p>blobloblo</p>"];
-var descViktor = ["", "VIKTOR", "??", "aaaaaa","blublublublu"];
+var descHong = ["Raven", "Hong", "56", "[REDACTED]","<p>Everyone around Lake Kivu knows who the forest hermit is, but no one really knows her.</p><p>A martial arts master and survival expert, Raven Hong will take on any guerrilla or army single-handedly to defend her people no matter how ruthless and cruel they may be.</p><p>Memories of her past make her realize daily that she was not so different from the people she fights against.</p>"];
+var descMagambo = ["Paul", "Magambo", "51", "Rwanda", "<p>Equal parts political leader, businessman and warlord, he heads the Movement for the Defense of the People, a rebel military group based in eastern DRC.</p><p>Thanks to the movement, Magambo controls important deposits of rare earths from which he has made his fortune.</p>"];
+var descEbba = ["Ebba", "Brower", "66", "Netherlands","<p>Director of the Brouwer Foundation, an NGO assigned in the DRC with the mission of denouncing human rights violations.</p><p>Her altruistic sensitivity leads her to help those most in need, although she can be deluded and overconfident.</p>"];
+var descThulani = ["", "Thulani", "??", "aaaaaa","<p>bliblibli</p>"];
+var descAsh = ["", "Ash", "??", "aaaaaa","<p>blobloblo</p>"];
+var descViktor = ["", "Viktor", "??", "aaaaaa","blublublublu"];
 
+//Preload images
+let allImages = document.getElementsByClassName("all-images")[0].children;
+
+var imageHong = allImages[0];
+var imageMagambo = allImages[1];
+var imageEbba = allImages[2];
+var imageThulani = allImages[3];
+var imageAsh = allImages[4];
+var imageViktor = allImages[5];
 
 
 //Functions
@@ -36,6 +45,12 @@ function selectCharacter(button){
     let charNation = charContent.getElementsByClassName("char-nationality")[0];
     let charDesc = charContent.getElementsByClassName("char-description")[0];
 
+    /*
+    charImage.onload = function(){
+        charContent.classList.remove("invisible");
+        charImage.classList.remove("invisible");
+    }*/
+
     this.setTimeout(function(){
         switch(button.dataset.character){
             case "Hong":
@@ -44,7 +59,7 @@ function selectCharacter(button){
                 charAge.innerHTML = descHong[2];
                 charNation.innerHTML = descHong[3];
                 charDesc.innerHTML = descHong[4];
-                charImage.src="./img/Hong/Characters/Hong.png";
+                charImage.src = imageHong.src;
                 break;
             case "Magambo":
                 charName.innerHTML = descMagambo[0];
@@ -52,7 +67,7 @@ function selectCharacter(button){
                 charAge.innerHTML = descMagambo[2];
                 charNation.innerHTML = descMagambo[3];
                 charDesc.innerHTML = descMagambo[4];
-                charImage.src="./img/Hong/Characters/Magambo.png";
+                charImage.src = imageMagambo.src;
                 break;
             case "Ebba":
                 charName.innerHTML = descEbba[0];
@@ -60,7 +75,7 @@ function selectCharacter(button){
                 charAge.innerHTML = descEbba[2];
                 charNation.innerHTML = descEbba[3];
                 charDesc.innerHTML = descEbba[4];
-                charImage.src="./img/Hong/Characters/Ebba.png";
+                charImage.src = imageEbba.src;
                 break;
             case "Thulani":
                 charName.innerHTML = descThulani[0];
@@ -68,7 +83,7 @@ function selectCharacter(button){
                 charAge.innerHTML = descThulani[2];
                 charNation.innerHTML = descThulani[3];
                 charDesc.innerHTML = descThulani[4];
-                charImage.src="./img/Hong/Characters/Thulani.png";
+                charImage.src = imageThulani.src;
                 break;
             case "Ash":
                 charName.innerHTML = descAsh[0];
@@ -76,7 +91,7 @@ function selectCharacter(button){
                 charAge.innerHTML = descAsh[2];
                 charNation.innerHTML = descAsh[3];
                 charDesc.innerHTML = descAsh[4];
-                charImage.src="./img/Hong/Characters/Ash.png";
+                charImage.src = imageAsh.src;
                 break;
             case "Viktor":
                 charName.innerHTML = descViktor[0];
@@ -84,14 +99,14 @@ function selectCharacter(button){
                 charAge.innerHTML = descViktor[2];
                 charNation.innerHTML = descViktor[3];
                 charDesc.innerHTML = descViktor[4];
-                charImage.src="./img/Hong/Characters/Viktor.png";
+                charImage.src = imageViktor.src;
                 break;
         }
-    
-        //charContent.insertBefore(charTitle, charDescription.firstChild);
 
         charContent.classList.remove("invisible");
         charImage.classList.remove("invisible");
+    
+        //charContent.insertBefore(charTitle, charDescription.firstChild);
     }, 200);
 
     let buttons = document.getElementById("char-selection").children;
